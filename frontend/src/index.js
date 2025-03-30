4fe462720b661d09f365246127e2be8044ab1ff5
JadKhalil <113618569+JadKhalil@users.noreply.github.com>
2025-03-30 01:52:32 -0700

    Updated File Directory to a more standard Organized hierarchy to work with react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-
-import Home from './components/home';
-import History from './components/history';
+import App from './routes/App'; // Optional if you're using App.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/history" element={<History />} />
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>
-);
+root.render(<App />);
