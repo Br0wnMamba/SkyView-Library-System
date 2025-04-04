225c4bb027b4952a9dd3bc646dccffba8ecc700c
adam-25 <adarsh.dudhat11@gmail.com>
2025-04-03 20:28:44 -0600

    add search and base of book overview

---
+++ frontend/src/pages/bookOverview/BookOverview.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "../../components/Search/Search";
+
+const BookOverview = () => {
+	  return (
+	<div>
+		<Search />
+	  <h1>Book Overview</h1>
+	  <p>This is the book overview page.</p>
+	</div>
+  );
+}
+
+export default BookOverview;
