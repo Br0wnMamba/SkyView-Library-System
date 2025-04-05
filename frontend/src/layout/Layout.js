081a1849ca97fc5008d70cff9ffd20872f60381e
lizzy-io <117678027+lizzy-io@users.noreply.github.com>
2025-04-05 17:27:43 -0600

    reverted layout to default

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -3,22 +3,11 @@ import "./Layout.css";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <div className="layout-container">
-      {/* Top nav or header */}
-      <header className="layout-header">
-        SkyView Public Library
-      </header>
-
-      {/* Main content from routing */}
-      <main className="layout-content">
-        <Outlet />
-      </main>
-
-      {/* Optional footer */}
-      <footer className="layout-footer">
-        &copy; 2025 SkyView Library System
-      </footer>
+      return (
+    <div>
+        <div>layout</div>
+      <Outlet/>
+      <div>layout</div>
     </div>
   );
 };
